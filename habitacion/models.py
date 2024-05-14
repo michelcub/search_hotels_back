5a245e2606ae9e39b3dc6afdf19701271df90a35
michel <orestelopez96@gmail.com>
2024-05-14 16:00:38 +0100

    add  model room type

--- habitacion/models.py
+++ habitacion/models.py
@@ -1,32 +1,46 @@
 from django.db import models
 
+
+class RoomType(models.Model):
+    hotel = models.ForeignKey('hotel.Hotel', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    max_customers = models.IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class Room(models.Model):
     number = models.CharField(max_length=100, blank=False, null=False, unique=True)
     description = models.TextField()
     hotel = models.ForeignKey('hotel.Hotel', on_delete=models.CASCADE)
-    CATEGORY = (
-        ('B', 'Basic'),
-        ('C', 'Confort'),
-        ('P', 'Premium'),
-    )
-    type = models.CharField(max_length=1, choices=CATEGORY)
+    type = models.ForeignKey(RoomType, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-
+    max_customers = models.IntegerField(default=1, null=False, blank=False)
+    
     def save(self, *args, **kwargs):
         if not self.price: 
-            self.price = self.calculate_price()
+            self.calculate_price()
+            self.set_max_customers()
+            self.set_description()
+            
         super().save(*args, **kwargs)
 
     def calculate_price(self):
-        if self.type == 'B':
-            return 170.00
-        elif self.type == 'C':
-            return 220.00
-        elif self.type == 'P':
-            return 298.00
-        else:
-            return 0.00
+        self.price = self.type.price
+    
+    def set_max_customers(self):
+        self.max_customers = self.type.max_customers
+    
+    def set_description(self):
+        self.description = self.type.description    
+    
 
+    def is_available(self, init_date, end_date):
+        from reservation.models import Reservation
+        return not Reservation.objects.filter(room=self, init_date__lte=end_date, end_date__gte=init_date).exists()
+        
     def __str__(self):
-        return self.name
+        return self.number + ' - ' + self.hotel.name + ' - ' + self.type.name + ' - ' + str(self.price)
