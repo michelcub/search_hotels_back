3e81d934e2051b937135a7086fda7e4c89dda98f
michel <orestelopez96@gmail.com>
2024-05-14 21:53:35 +0100

    change serializer to roomtype with include hotel serializer

--- habitacion/api/serializer.py
+++ habitacion/api/serializer.py
@@ -2,9 +2,10 @@ from rest_framework import serializers
 
 from habitacion.models import Room
 from habitacion.models import RoomType
-
+from hotel.api.serializer import HotelSerializer
 
 class RoomTypeSerializer(serializers.ModelSerializer):
+    hotel = HotelSerializer()
     class Meta:
         model = RoomType
         fields = '__all__'
